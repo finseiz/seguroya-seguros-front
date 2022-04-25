2865c18cccd73c24ec0fe02edf9b19e99ac09350
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2022-04-25 10:49:02 -0500

    lit typos fix

--- src/app/modules/auth/components/Login.jsx
+++ src/app/modules/auth/components/Login.jsx
@@ -1,115 +1,105 @@
-import React, { useState } from 'react'
-import FormikInput from 'app/modules/_forms/general/FormikInput'
-import { useFormik } from 'formik';
-import { toAbsoluteUrl } from 'theme/helpers/AssetsHelpers'
-import FormikSelect from 'app/modules/_forms/general/FormikSelect';
-import { createAuthData, login } from '../helpers/controller';
-import { loginSchema } from '../helpers/formik';
-import { useHistory } from 'react-router';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import FormikInput from "app/modules/_forms/general/FormikInput";
+import { useFormik } from "formik";
+import { toAbsoluteUrl } from "theme/helpers/AssetsHelpers";
+import FormikSelect from "app/modules/_forms/general/FormikSelect";
+import { createAuthData, login } from "../helpers/controller";
+import { loginSchema } from "../helpers/formik";
+import { useHistory } from "react-router";
+import { useDispatch } from "react-redux";
 
 export const Login = () => {
+  const [requestStatus, setRequestStatus] = useState({
+    loading: false,
+    error: "",
+  });
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const [requestStatus, setRequestStatus] = useState({ loading: false, error: "" });
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: { documentType: "", document: "", password: "" },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      setRequestStatus({ loading: true, error: "" });
+      const data = createAuthData(values);
+      login(data, dispatch).then((data) => {
+        if (data.error) {
+          setRequestStatus({ loading: false, error: data.error });
+        } else {
+          history.push("/");
+        }
+      });
+    },
+  });
 
-    const formik = useFormik({
-        initialValues: { documentType: "", document: "", password: "" },
-        validationSchema: loginSchema,
-        onSubmit: (values) => {
-            setRequestStatus({ loading: true, error: "" })
-            const data = createAuthData(values)
-            login(data, dispatch)
-                .then(data => {
-                    if (data.error) {
-                        setRequestStatus({ loading: false, error: data.error })
-                    } else {
-                        history.push("/")
-                    }
-
-                })
-        },
-    });
-
-
-
-    return (
-        <div className="center-flex text-center auth-login-container">
-            <form className="w-75" onSubmit={formik.handleSubmit}>
-
-                <div className= "mt-4">
-                    <img
-                        className="logo"
-                        src={toAbsoluteUrl("/media/logos/logo_seguroya_dark.svg")}
-                    />
-
-                </div>
-
-                <div className="bg-white rounded mt-2 mx-auto">
-
-                    <p className="auth-title pt-4"> Iniciar sesión </p>
-
-                    <div className="w-50 m-auto text-left">
-
-                        {
-                            requestStatus.error && 
-                            (
-                                <div className="alert alert-danger text-center" role="alert">
-                                    {requestStatus.error}
-                                </div>
-                            )
-                        }
-
-                        <div className="">
-                            <FormikSelect
-                                formik={formik}
-                                field="documentType"
-                                label="Tipo de documento"
-                                options={[
-                                    { title: "Seleccionar", value: "" },
-                                    { title: "Cédula", value: "CC" },
-                                    { title: "Cédila de extranjería", value: "CE" },
-                                ]}
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                            <FormikInput formik={formik} field="document" label="Documento" type="number" />
-                        </div>
-
-                        <div className="mt-4">
-                            <FormikInput formik={formik} field="password" label="Contraseña" type="password" />
-                        </div>
-
-
-                        {
-                            requestStatus.loading ?
-                                (
-                                    <div className="spinner-border my-5" role="status">
-                                        <span className="visually-hidden"></span>
-                                    </div>
-                                ) :
-                                (
-                                    <>
-                                        <button
-                                            type="submit"
-                                            className="btn btn-primary primary-button w-100 my-5"
-                                        >
-                                            <b>Iniciar sesión</b>
-                                        </button>
-                                    </>
-                                )
-                        }
-
-
-                    </div>
-
-                </div>
-
-            </form>
-
+  return (
+    <div className="center-flex text-center auth-login-container">
+      <form className="w-75" onSubmit={formik.handleSubmit}>
+        <div className="mt-4">
+          <img
+            className="logo"
+            src={toAbsoluteUrl("/media/logos/logo_seguroya_dark.svg")}
+          />
         </div>
 
-    )
-}
+        <div className="bg-white rounded mt-2 mx-auto">
+          <p className="auth-title pt-4"> Iniciar sesión </p>
+
+          <div className="w-50 m-auto text-left">
+            {requestStatus.error && (
+              <div className="alert alert-danger text-center" role="alert">
+                {requestStatus.error}
+              </div>
+            )}
+
+            <div className="">
+              <FormikSelect
+                formik={formik}
+                field="documentType"
+                label="Tipo de documento"
+                options={[
+                  { title: "Seleccionar", value: "" },
+                  { title: "Cédula", value: "CC" },
+                  { title: "Cédula de extranjería", value: "CE" },
+                ]}
+              />
+            </div>
+
+            <div className="mt-4">
+              <FormikInput
+                formik={formik}
+                field="document"
+                label="Documento"
+                type="number"
+              />
+            </div>
+
+            <div className="mt-4">
+              <FormikInput
+                formik={formik}
+                field="password"
+                label="Contraseña"
+                type="password"
+              />
+            </div>
+
+            {requestStatus.loading ? (
+              <div className="spinner-border my-5" role="status">
+                <span className="visually-hidden"></span>
+              </div>
+            ) : (
+              <>
+                <button
+                  type="submit"
+                  className="btn btn-primary primary-button w-100 my-5"
+                >
+                  <b>Iniciar sesión</b>
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/theme/layout/components/customNavbar.jsx
+++ src/theme/layout/components/customNavbar.jsx
@@ -1,28 +1,34 @@
 import { actions } from "app/modules/auth/_redux/authRedux";
 import { LoginRoute, RegistryRoute } from "app/routes/childs/Auth/routes";
 import { UserPurchasesRoute } from "app/routes/childs/User/routes";
-import { AboutUsRoute, FrequentQuestionsRoute, HomeRoute, OffersRoute,SarlaftRoute } from "app/routes/routes";
+import {
+  AboutUsRoute,
+  FrequentQuestionsRoute,
+  HomeRoute,
+  OffersRoute,
+  SarlaftRoute,
+} from "app/routes/routes";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { toAbsoluteUrl } from "../../helpers";
 
 function CustomNavbar() {
-
-  const { user, authToken } = useSelector(state => state.auth);
+  const { user, authToken } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const isAuthenticated = authToken !== undefined;
   const userName = user?.nombres ?? "";
 
   const logout = () => dispatch(actions.logout());
-  
 
   return (
     <div>
-
       <div className="container-fluid bg-white">
-        <a href="https://api.whatsapp.com/message/QOFDDFAB7GWIE1" target="_blank">
+        <a
+          href="https://api.whatsapp.com/message/QOFDDFAB7GWIE1"
+          target="_blank"
+        >
           <div className="whatsapp-contact text-center">
             <img
               src={toAbsoluteUrl("/media/icons/Aceptado.png")}
@@ -35,18 +41,22 @@ function CustomNavbar() {
       </div>
 
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
-
-        <div className="container-fluid"> 
-
+        <div className="container-fluid">
           {/* Seguro Ya Brand */}
           <NavLink to="/" className="navbar-brand">
-            <img
-              src={toAbsoluteUrl("/media/logos/logo_seguroya_dark.svg")}
-            />{" "}
+            <img src={toAbsoluteUrl("/media/logos/logo_seguroya_dark.svg")} />{" "}
           </NavLink>
 
           {/* Toggle Buttoms */}
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
 
@@ -54,102 +64,141 @@ function CustomNavbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <div className="mr-auto">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
- 
                 <li className="nav-item">
-                  <NavLink to={HomeRoute} className="nav-link" activeClassName="active" >
+                  <NavLink
+                    to={HomeRoute}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
                     Inicio
                   </NavLink>
                 </li>
 
                 <li className="nav-item">
-                  <NavLink to={AboutUsRoute} className="nav-link" activeClassName="active" >
+                  <NavLink
+                    to={AboutUsRoute}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
                     ¿Quiénes somos?
                   </NavLink>
                 </li>
 
                 <li className="nav-item">
-                  <NavLink to={OffersRoute} className="nav-link" activeClassName="active" >
+                  <NavLink
+                    to={OffersRoute}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
                     Ofertas
                   </NavLink>
                 </li>
 
                 <li className="nav-item">
-                  <NavLink to={FrequentQuestionsRoute} className="nav-link" activeClassName="active" >
+                  <NavLink
+                    to={FrequentQuestionsRoute}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
                     Preguntas frecuentes
                   </NavLink>
                 </li>
 
-                {
-                  isAuthenticated &&
-                  (
-                    <li className="nav-item">
-                      <NavLink to={UserPurchasesRoute} className="nav-link" activeClassName="active" >
-                        Mi biblioteca
-                      </NavLink>
-                    </li>
-                  )
-                }
+                {isAuthenticated && (
+                  <li className="nav-item">
+                    <NavLink
+                      to={UserPurchasesRoute}
+                      className="nav-link"
+                      activeClassName="active"
+                    >
+                      Mi biblioteca
+                    </NavLink>
+                  </li>
+                )}
               </ul>
-
             </div>
 
             <div>
-
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-                {
-                  isAuthenticated ?
-                  (
-                    <div>
-                      <div className="dropdown">
-                        <button className="btn btn-secondary dropdown-toggle dropdown" type="button" id="userSessionDropD" data-bs-toggle="dropdown" aria-expanded="false">
-                          <img
-                            src={toAbsoluteUrl(`/media/icons/persona.svg`)}
-                            className="dropdown__icon"
-                          />
-                          ¡Bienvenid@, {userName}!
-                        </button>
-                        <ul className="dropdown-menu" aria-labelledby="userSessionDropD">
-                          <li>
-                            <NavLink to={SarlaftRoute} className="nav-link" activeClassName="active" >
-                              <span className="dropdown-item" > Llenar Sarlaf</span>
-                            </NavLink>
-                          </li>
-                          <li><span className="dropdown-item" onClick={() => logout()}>Cerrar sesión</span></li>
-                        </ul>
-                      </div>
+                {isAuthenticated ? (
+                  <div>
+                    <div className="dropdown">
+                      <button
+                        className="btn btn-secondary dropdown-toggle dropdown"
+                        type="button"
+                        id="userSessionDropD"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        <img
+                          src={toAbsoluteUrl(`/media/icons/persona.svg`)}
+                          className="dropdown__icon"
+                        />
+                        ¡Bienvenid@, {userName}!
+                      </button>
+                      <ul
+                        className="dropdown-menu"
+                        aria-labelledby="userSessionDropD"
+                      >
+                        <li>
+                          <NavLink
+                            to={SarlaftRoute}
+                            className="nav-link"
+                            activeClassName="active"
+                          >
+                            <span className="dropdown-item">
+                              {" "}
+                              Llenar Sarlaf
+                            </span>
+                          </NavLink>
+                        </li>
+                        <li>
+                          <span
+                            className="dropdown-item"
+                            onClick={() => logout()}
+                          >
+                            Cerrar sesión
+                          </span>
+                        </li>
+                      </ul>
                     </div>
-                  ) :
-                  (
-                    <>
-                      <li className="nav-item">
-                        <NavLink to={LoginRoute} className="nav-link" activeClassName="active" >
-                          <div className="btn btn-secondary secondary-button"> Iniciar Sesión </div>
-                        </NavLink>
-                      </li>
+                  </div>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <NavLink
+                        to={LoginRoute}
+                        className="nav-link"
+                        activeClassName="active"
+                      >
+                        <div className="btn btn-secondary secondary-button">
+                          {" "}
+                          Iniciar Sesión{" "}
+                        </div>
+                      </NavLink>
+                    </li>
 
-                      <li className="nav-item">
-                        <NavLink to={RegistryRoute} className="nav-link" activeClassName="active" >
-                          <div className="btn btn-secondary primary-button"> Registarse </div>
-                        </NavLink>
-                      </li>
-                    </>
-                  )
-
-                }
-
+                    <li className="nav-item">
+                      <NavLink
+                        to={RegistryRoute}
+                        className="nav-link"
+                        activeClassName="active"
+                      >
+                        <div className="btn btn-secondary primary-button">
+                          {" "}
+                          Registrarse{" "}
+                        </div>
+                      </NavLink>
+                    </li>
+                  </>
+                )}
               </ul>
-
             </div>
-
           </div>
-
         </div>
-
       </nav>
-
     </div>
-  )
+  );
 }
 
 export default CustomNavbar;
