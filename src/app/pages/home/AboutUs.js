a1070a8fdb4b986cece50340c1541eac752d3815
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2022-06-09 22:56:16 -0500

    add about us section image

--- src/app/pages/home/AboutUs.js
+++ src/app/pages/home/AboutUs.js
@@ -1,36 +1,59 @@
-import React from 'react'
+import React from "react";
 
 export const AboutUs = () => {
-    return (
-        <div className="container text-left">
-            
-            <div className="row mt-5">
-
-                <div className="col-sm">
-
-                </div>
-
-                <section className="col-sm">
-
-                    <h1 className="home-pages__title mt-4"> ¿Quiénes somos? </h1>
-
-                    <div> </div>
-
-                    <div className="home-pages__content mt-4">
-
-                        <p> Somos un agencia colombiana de seguros, que busca revolucionar la industria de los seguros a través del mercado digital, ofrecemos un servicio rápido, amigable y confiable, 100% virtual y con asesoria 24/7. Nuestro proposito es entregar soluciones ajustadas a las necesidades de cada cliente, para brindar tranquilidad al momento de una situación adversa inesperada. </p>
-
-                        <b><p>Contacto</p></b>
-
-                        <p> <b>Celular: </b> 3204056381 </p>
-                        <p> <b>Correo: </b> info@seguroya.co </p>
-                        <p> <b>Dirección: </b> Calle13C #70-87 Cali </p>
-                    </div>
-
-                </section>
-
-            </div>
-
+  return (
+    <div className="container text-left">
+      <div className="row mt-5">
+        <div
+          style={{
+            display: "grid",
+            alignContent: "center",
+          }}
+          className="col-sm"
+        >
+          <img
+            width="100%"
+            src="/media/images/about_us.jpeg"
+            alt=""
+            srcset=""
+          />
         </div>
-    )
-}
+
+        <section className="col-sm">
+          <h1 className="home-pages__title mt-4"> ¿Quiénes somos? </h1>
+
+          <div> </div>
+
+          <div className="home-pages__content mt-4">
+            <p>
+              {" "}
+              Somos un agencia colombiana de seguros, que busca revolucionar la
+              industria de los seguros a través del mercado digital, ofrecemos
+              un servicio rápido, amigable y confiable, 100% virtual y con
+              asesoria 24/7. Nuestro proposito es entregar soluciones ajustadas
+              a las necesidades de cada cliente, para brindar tranquilidad al
+              momento de una situación adversa inesperada.{" "}
+            </p>
+
+            <b>
+              <p>Contacto</p>
+            </b>
+
+            <p>
+              {" "}
+              <b>Celular: </b> 3204056381{" "}
+            </p>
+            <p>
+              {" "}
+              <b>Correo: </b> info@seguroya.co{" "}
+            </p>
+            <p>
+              {" "}
+              <b>Dirección: </b> Calle13C #70-87 Cali{" "}
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
--- src/app/pages/home/Offers.js
+++ src/app/pages/home/Offers.js
@@ -1,94 +1,85 @@
-import React, { useMemo } from 'react'
+import React, { useMemo } from "react";
 import { NavLink } from "react-router-dom";
 
 export const Offers = () => {
- 
-    const offersList = useMemo(() => [
-        {
-            img: "/media/offers/of1.jpg",
-            benefits: [
-                "No pagas prima cuando el vehículo está detenido.",
-                "Disminución en la prima del seguro por buenos comportamientos de conducción.",
-                "Dispositivo para el rastreo del vehículo en caso de hurto."
-            ],
-           
-            path:"/cars",
-        },
-        {
-            img: "/media/offers/of2.jpg",
-            benefits: [
-                "Planes desde $25.000 pesos mensuales."
-            ],
-            path:"/life",
-        },
-        // {
-        //     img: "/media/offers/of3.jpg",
-            // benefits: [
-            //     "Accede a un médico especialista en cualquier horario y desde cualquier lugar.",
-            //     "Sin filas, sin largas esperas y con la tranquilidad de no tener riesgos de contagio de COVID-19.",
-            //     "Sin restricción de edad. ",
+  const offersList = useMemo(
+    () => [
+      {
+        img: "/media/offers/of_1.png",
+        benefits: [
+          "No pagas prima cuando el vehículo está detenido.",
+          "Disminución en la prima del seguro por buenos comportamientos de conducción.",
+          "Dispositivo para el rastreo del vehículo en caso de hurto.",
+        ],
 
-            // ],
-        //     path:"/health",
-        // },
-    ], [])
+        path: "/cars",
+      },
+      {
+        img: "/media/offers/of_2.png",
+        benefits: ["Planes desde $25.000 pesos mensuales."],
+        path: "/life",
+      },
+      // {
+      //     img: "/media/offers/of3.jpg",
+      // benefits: [
+      //     "Accede a un médico especialista en cualquier horario y desde cualquier lugar.",
+      //     "Sin filas, sin largas esperas y con la tranquilidad de no tener riesgos de contagio de COVID-19.",
+      //     "Sin restricción de edad. ",
 
-    return (
-        <div className="container-fluid text-center">
+      // ],
+      //     path:"/health",
+      // },
+    ],
+    []
+  );
 
-            <h1 className="home-pages__title mt-4"> Ofertas </h1>
+  return (
+    <div className="container-fluid text-center">
+      <h1 className="home-pages__title mt-4"> Ofertas </h1>
 
-            <div className="d-inline-flex flex-wrap row">
-                {
-                    offersList.map(offer => (
-                        <div className="col-4 mt-4 ">
-                                <div className="bg-white home-offer__image-container">
-                                    <NavLink to={offer.path}>
-                                        <img
-                                            src={offer.img} 
-                                            className="home-offer__image"
-                                        />
-
-                                    </NavLink>
-                                    <ul className="py-4 text-left home-offer__text">
-                                        {
-                                            offer.benefits.map(benefit => (
-                                                <li> {benefit} </li>
-                                            )
-                                            )
-                                        }
-                                    </ul>
-                                </div>
-                            </div>
-                    ))
-                }
-
-                    <div className="col-4 mt-4 ">
-                        <div className="bg-white home-offer__image-container">                            
-                            <a href="https://api.whatsapp.com/message/QOFDDFAB7GWIE1" target="_blank">
-                                <img
-                                    src="/media/offers/of3.jpg"
-                                    className="home-offer__image"
-                                />                            
-                            </a>
-                            <ul className="py-4 text-left home-offer__text">
-                                {                                                                        
-                                    [
-                                        "Accede a un médico especialista en cualquier horario y desde cualquier lugar.",
-                                        "Sin filas, sin largas esperas y con la tranquilidad de no tener riesgos de contagio de COVID-19.",
-                                        "Sin restricción de edad. ",
-                        
-                                    ].map(benefit => (
-                                        <li> {benefit} </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                    </div>
+      <div
+        style={{ gap: "2rem", justifyContent: "center" }}
+        className="d-inline-flex flex-wrap row"
+      >
+        {offersList.map((offer) => (
+          <div
+            style={{
+              display: "grid",
+            }}
+          >
+            <div className="bg-white home-offer__image-container">
+              <NavLink to={offer.path}>
+                <img src={offer.img} className="home-offer__image" />
+              </NavLink>
+              <ul className="py-4 text-left home-offer__text">
+                {offer.benefits.map((benefit) => (
+                  <li> {benefit} </li>
+                ))}
+              </ul>
             </div>
+          </div>
+        ))}
 
-
-
-        </div>
-    )
-}
+        {/* <div className="col-4 mt-4 ">
+          <div className="bg-white home-offer__image-container">
+            <a
+              href="https://api.whatsapp.com/message/QOFDDFAB7GWIE1"
+              target="_blank"
+            >
+              <img src="/media/offers/of3.jpg" className="home-offer__image" />
+            </a>
+            <ul className="py-4 text-left home-offer__text">
+              {[
+                "Accede a un médico especialista en cualquier horario y desde cualquier lugar.",
+                "Sin filas, sin largas esperas y con la tranquilidad de no tener riesgos de contagio de COVID-19.",
+                "Sin restricción de edad. ",
+              ].map((benefit) => (
+                <li> {benefit} </li>
+              ))}
+            </ul>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
